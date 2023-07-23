7f66908d75d6e581816b6b526ffa51c4747d8ced
Лиза Герман <liza36046@gmail.com>
2023-07-23 11:31:39 +0300

    Initial commit

---
+++ src/abc.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+
+
+class JobSearchService(ABC):
+
+    @abstractmethod
+    def get_vacancies(self, search):
+        pass
+
+    @abstractmethod
+    def insert_vacancies(self, name_vacancy, salary_from, found_vacancies):
+        pass
+
+
+class AbstractFile(ABC):
+
+    @abstractmethod
+    def add_vacancy(self, new_data):
+        pass
+
+
+def ABC():
+    return None
+
+def abstractmethod():
+    return None
